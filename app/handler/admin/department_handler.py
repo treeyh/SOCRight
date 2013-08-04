40af091102dfd4f5f75b530485120d277da1ae10
Tree <yuhai717@163.com>
2013-08-04 21:16:45 +0800

    添加缺失文件

---
+++ app/handler/admin/department_handler.py
@@ -0,0 +1,122 @@
+#-*- encoding: utf-8 -*-
+
+
+import tornado.web
+import tornado.escape
+import config
+
+from datetime import datetime, timedelta
+import admin_base_handler
+from common import redis_cache, state, error, log
+from helper import str_helper, http_helper
+from logic import department_logic
+
+class DepartmentListHandler(admin_base_handler.AdminRightBaseHandler):
+    _rightKey = config.SOCRightConfig['appCode'] + '.DepartmentManager'
+    _right = state.operView
+
+    def get(self):
+        ps = self.get_page_config('部门列表')
+        dep = {}
+        dep['name'] = self.get_arg('name', '')
+        dep['status'] = int(self.get_arg('status', '0'))
+        ps['page'] = int(self.get_arg('page', '1'))
+        ps['pagedata'] = department_logic.DepartmentLogic.instance().query_page(name = dep['name'], 
+                        status= dep['status'], page = ps['page'], size = ps['size'])
+        ps['dep'] = dep
+        ps['pager'] = self.build_page_html(page = ps['page'], size = ps['size'], total = ps['pagedata']['total'], pageTotal = ps['pagedata']['pagetotal'])        
+        self.render('admin/department/list.html', **ps)
+
+class DepartmentAddOrEditHandler(admin_base_handler.AdminRightBaseHandler):
+    _rightKey = config.SOCRightConfig['appCode'] + '.DepartmentManager'
+    _right = 0
+
+    def get(self):
+        ps = self.get_page_config('创建部门')
+        if ps['isedit']:
+            self.check_oper_right(right = state.operEdit)
+
+            ps['title'] = self.get_page_title('编辑部门')
+            id = int(self.get_arg('id', '0'))
+            dep = department_logic.DepartmentLogic.instance().query_one(id = id)
+            if None == dep:
+                ps['msg'] = state.ResultInfo.get(1002, '')
+                ps['gotoUrl'] = ps['siteDomain'] + 'Admin/Department/List'
+                dep = {'id':'','name':'','remark':'','status':1,'creater':'','createTime':'','lastUpdater':'','lastUpdateTime':''}
+        else:
+            self.check_oper_right(right = state.operAdd)
+            dep = self.get_args(['name', 'remark'], '')
+            dep['status'] = int(self.get_arg('status', '0'))        
+        ps['dep'] = dep
+        self.render('admin/department/add_or_edit.html', **ps)
+
+    def post(self):
+        ps = self.get_page_config('创建部门')
+        if ps['isedit']:
+            ps['title'] = self.get_page_title('编辑部门')
+
+        dep = self.get_args(['name', 'remark'], '')
+        dep['status'] = int(self.get_arg('status', '0'))
+        dep['id'] = int(self.get_arg('id', '0'))
+        ps['dep'] = dep        
+        msg = self.check_str_empty_input(dep, ['name'])
+        if str_helper.is_null_or_empty(msg) == False:
+            ps['msg'] = msg
+            self.render('admin/department/add_or_edit.html', **ps)
+            return
+        dep['user'] = self.get_oper_user()
+        if ps['isedit']:
+            self.check_oper_right(right = state.operEdit)
+
+            try:
+                info = department_logic.DepartmentLogic.instance().update(id = dep['id'], name = dep['name'], 
+                    status = dep['status'], remark = dep['remark'], user = dep['user'])
+                
+                if info:
+                    ps = self.get_ok_and_back_params(ps = ps)
+                else:
+                    ps['msg'] = state.ResultInfo.get(101, '')
+            except error.RightError as e:
+                ps['msg'] = e.msg
+        else:
+            self.check_oper_right(right = state.operAdd)
+            try:
+                info = department_logic.DepartmentLogic.instance().add(name = dep['name'],
+                        status = dep['status'], remark = dep['remark'], user = dep['user'])
+                if info:
+                    ps = self.get_ok_and_back_params(ps = ps)
+                else:
+                    ps['msg'] = state.ResultInfo.get(101, '')
+            except error.RightError as e:
+                ps['msg'] = e.msg
+        ps = self.format_none_to_empty(ps)
+        self.render('admin/department/add_or_edit.html', **ps)
+
+
+
+class DepartmentDelHandler(admin_base_handler.AdminRightBaseHandler):
+    _rightKey = config.SOCRightConfig['appCode'] + '.DepartmentManager'
+    _right = state.operDel
+    def post(self):
+        code = self.get_arg('code', '')
+        user = self.get_oper_user()
+        type = application_logic.ApplicationLogic.instance().delete(code = code, user = user)
+        if type:
+            self.out_ok()
+        else:
+            self.out_fail(code = 101)
+
+class DepartmentDetailHandler(admin_base_handler.AdminRightBaseHandler):
+    _rightKey = config.SOCRightConfig['appCode'] + '.DepartmentManager'
+    _right = state.operView
+    def get(self):
+        ps = self.get_page_config('部门详情')
+        id = int(self.get_arg('id', '0'))
+        dep = department_logic.DepartmentLogic.instance().query_one(id = id)
+        if None == dep:
+            ps['msg'] = state.ResultInfo.get(1002, '')
+            ps['gotoUrl'] = ps['siteDomain'] + 'Admin/Department/List'
+            app = {'name':'','remark':'','status':1,'creater':'','createTime':'','lastUpdater':'','lastUpdateTime':''}
+        
+        ps['dep'] = dep
+        self.render('admin/department/detail.html', **ps)
---
+++ app/logic/department_logic.py
@@ -0,0 +1,121 @@
+#-*- encoding: utf-8 -*-
+
+from helper import str_helper
+from common import mysql, state, error
+
+class DepartmentLogic():
+
+    def __init__(self):   
+        return
+
+    _instance = None
+    @classmethod
+    def instance(cls):
+        if cls._instance == None:
+            cls._instance = cls()
+        return cls._instance
+
+
+
+    _query_sql = '''  select  id, `name`, `status`, remark, isDelete, 
+                        creater, createTime, lastUpdater, lastUpdateTime  
+                        from  sso_department   where  isDelete = %s   '''
+    _query_col = str_helper.format_str_to_list_filter_empty('id, name, status, remark, isDelete, creater, createTime, lastUpdater, lastUpdateTime', ',')
+    ''' 分页查询部门信息 '''
+    def query_page(self, name = '', status = 0, page = 1 , size = 12):        
+        sql = self._query_sql
+        isdelete = state.Boole['false']
+        ps = [isdelete]
+        if 0 != status:
+            sql = sql + ' and status = %s '
+            ps.append(status)
+        if '' != name:
+            sql = sql + ' and name like %s '
+            ps.append('%'+name+'%')
+
+        sql = sql + ' order by createTime asc '
+        yz = tuple(ps)
+        deps = mysql.find_page(sql, yz, self._query_col, page, size)
+        if None != deps['data']:
+            for r in deps['data']:
+                r['statusname'] = state.Status.get(r['status'])
+        return deps
+ 
+    ''' 根据编号查询部门信息 '''
+    def query_one(self, id = 0):
+        sql = self._query_sql
+        isdelete = state.Boole['false']
+        ps = [isdelete]        
+        if 0 != id:
+            sql = sql + ' and id = %s '
+            ps.append(id)
+        else:
+            return None
+        yz = tuple(ps)
+        dep = mysql.find_one(sql, yz, self._query_col)
+        if None != dep:
+            dep['statusname'] = state.Status.get(dep['status'])
+        return dep
+
+    ''' 根据名称查询部门信息 '''
+    def query_one_by_name(self, name = ''):
+        sql = self._query_sql
+        isdelete = state.Boole['false']
+        sql = sql + ' and name = %s '
+        yz = (isdelete, name)
+        dep = mysql.find_one(sql, yz, self._query_col)
+        if None != dep:
+            dep['statusname'] = state.Status.get(dep['status'])
+        return dep
+
+    _query_all_by_active_sql = '''  select id, name from sso_department  where isDelete = %s   '''
+    _query_all_by_active_col = str_helper.format_str_to_list_filter_empty('id, name', ',')
+    ''' 查询所有可用的部门 '''
+    def query_all_by_active(self):
+        sql = self._query_all_by_active_sql
+
+        isdelete = state.Boole['false']
+        sql = sql + ' and status = %s order by createTime asc '        
+        yz = (isdelete, state.statusActive)
+        deps = mysql.find_all(sql, yz, self._query_all_by_active_col)
+        return deps
+
+
+    _add_sql = '''   INSERT INTO sso_department(name, status, remark, 
+                         isDelete, creater, createTime, lastUpdater, lastUpdateTime)  
+                        VALUES(%s, %s, %s, %s, %s, now(), %s, now())  '''
+    ''' 添加部门 '''
+    def add(self, name, status, remark, user):
+        obj = self.query_one_by_name(name = name)
+        if None != obj:
+            raise error.RightError(code = 106001)
+            
+        isdelete = state.Boole['false']
+        yz = (name, status, remark, isdelete, user, user)
+        result = mysql.insert_or_update_or_delete(self._add_sql, yz)
+        return 0 == result
+
+
+    _update_sql = '''   update sso_department set name = %s, status = %s, 
+                            remark = %s, lastUpdater = %s, 
+                            lastUpdateTime = now() where id = %s  '''
+    ''' 更新部门 '''
+    def update(self, id, name, status, remark, user):
+        obj = self.query_one_by_name(name = name)
+        if None != obj and str(obj['id']) != str(id):
+            raise error.RightError(code = 106001)
+
+        isdelete = state.Boole['false']
+        yz = (name, status, remark, user, id)
+        result = mysql.insert_or_update_or_delete(self._update_sql, yz)
+        return 0 == result
+
+    
+    _delete_sql = '''   update sso_department set isDelete = %s, lastUpdater = %s, 
+                            lastUpdateTime = now() where id = %s  '''
+    ''' 删除部门 '''
+    def delete(self, id, user):
+        isdelete = state.Boole['true']
+        yz = (isdelete, user, id)
+        result = mysql.insert_or_update_or_delete(self._delete_sql, yz)
+        return 0 == result
